0b8a3e0cdc3c7aa2ba2396729ca7482b1b6857f5
Wojtek <wojciechkrzywiec@gmail.com>
2018-09-04 07:23:32 +0200

    PathVariable

--- src/main/java/com/wkrzywiec/simplespringmvc/controller/SimpleSpringMVCController.java
+++ src/main/java/com/wkrzywiec/simplespringmvc/controller/SimpleSpringMVCController.java
@@ -2,13 +2,26 @@ package com.wkrzywiec.simplespringmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class SimpleSpringMVCController {
 
-	@GetMapping("/home")
+	@GetMapping("/")
 	public String showHomePage() {
 		
 		return "home";
+	}
+	
+	@GetMapping("/meme/{name}")
+	public String showNotsure(	@PathVariable("name") String name) {
+		
+		if (name.equals("notsure")) {
+			return "notsure";
+		} else if (name.equals("instead")) {
+			return "instead";
+		} else {
+			return "works";
+		}
 	}
 }
